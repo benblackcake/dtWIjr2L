426eba4e12a89a45bad75782267b7cca162440f5
benblackcake <ben.chang@aiesec.net>
2019-06-06 00:05:11 +0800

    fix private method

--- x_1/applePhone.py
+++ x_1/applePhone.py
@@ -3,10 +3,10 @@ from math import factorial
 
 class applePhone(phone):
     def special_freature(self,n,m):
-        return self.calc_arrangement(n,m)
+        return self._calc_arrangement(n,m)
 
 
-    def calc_arrangement(self,n, m):
+    def _calc_arrangement(self,n, m):
         return factorial(n)/factorial(n-m)
 
 
--- x_1/taiwanPhone.py
+++ x_1/taiwanPhone.py
@@ -3,10 +3,10 @@ from BasePhone import phone
 class taiwanPhone(phone):
 
     def special_freature(self,n):
-        return self.fib(n)
+        return self._fib(n)
 
-    def fib(self,n):
+    def _fib(self,n):
         if n == 0 or n == 1:
             return n
         else:
-            return self.fib(n-1)+self.fib(n-2)
+            return self._fib(n-1)+self._fib(n-2)
