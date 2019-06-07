eada523566494b1bd85e1a6ded4823179de58257
benblackcake <ben.chang@aiesec.net>
2019-06-07 20:54:15 +0800

    希望有機會可以進入這麼重視人才的公司上班,可以和team上的前輩學習到更多程式上的技術

--- x_2/main.py
+++ x_2/main.py
@@ -4,10 +4,10 @@ import time
 import random
 
 def loop():
-    for i in range(0,9):
+    for i in range(10):
         print(M1.__Attr_X__)
         t=random.randint(1, 10)
-        time.sleep(t)
+        #time.sleep(t)
         print("sleep",t,"s")
         reload(M1)
 
